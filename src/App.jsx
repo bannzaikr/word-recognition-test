cf20ca766b6363efa5550beb644cca32925871c1
bannzaikr <park@hus-et.net>
2024-11-18 22:56:52 +0900

    Fix Progress component location

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { Card } from './components/ui/card'
-import { Button } from './components/ui/button'
-import { Input } from './components/ui/input'
+import WordRecognitionTest from './components/WordRecognitionTest'
 
 function App() {
-  return (
-    <div className="p-4">
-      <Card className="p-6">
-        <h1 className="text-2xl mb-4">テスト</h1>
-        <Input className="mb-4" placeholder="テスト入力" />
-        <Button>テストボタン</Button>
-      </Card>
-    </div>
-  )
+  return <WordRecognitionTest />
 }
 
 export default App
